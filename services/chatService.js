ce6da76ae94b718cad3aef5f5ddd037f87a6df1e
REDSHADOW-TH <59532816+REDSHADOW-TH@users.noreply.github.com>
2021-01-30 06:56:43 +0700

    tes

--- services/chatService.js
+++ services/chatService.js
@@ -5,7 +5,6 @@ const request = require('request')
 const api = 'https://graph.facebook.com/v9.0/me/messages'
 
 function callSendAPI(sender_psid, response) {
-    // Construct the message body
     let request_body = {
       "recipient": {
         "id": sender_psid
@@ -14,7 +13,6 @@ function callSendAPI(sender_psid, response) {
           "text": response
       }
     }
-    // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
     "qs": { "access_token": config.vertifyTokens },
@@ -22,6 +20,7 @@ function callSendAPI(sender_psid, response) {
     "json": request_body
   }, (err, res, body) => {
     if (!err) {
+        console.log(body)
       console.log('message sent!')
     } else {
       console.error("Unable to send message:" + err);
